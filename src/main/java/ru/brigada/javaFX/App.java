3602dc303218a671f69ecc75734120a43f66016d
denis <soloshenko-den@@mail.ru>
2023-10-08 21:41:39 +0700

    init_denis

--- src/main/java/ru/brigada/javaFX/App.java
+++ src/main/java/ru/brigada/javaFX/App.java
@@ -1,13 +1,18 @@
 package ru.brigada.javaFX;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+    public class App extends Application {
+        @Override
+        public void start(Stage primaryStage) throws Exception {
+            primaryStage.setTitle("My First JavaFX App");
+            primaryStage.show();
+        }
+
+        public static void main(String[] args) {
+            Application.launch(args);
+        }
+
     }
-}
+
