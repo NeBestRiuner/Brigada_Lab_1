c119a904f8f50d6f4a264f04e976a046aa0978f3
denis <soloshenko-den@@mail.ru>
2023-10-19 22:21:16 +0700

    denis_it_works

--- src/main/java/ru/brigada/javaFX/App.java
+++ src/main/java/ru/brigada/javaFX/App.java
@@ -2,11 +2,17 @@ package ru.brigada.javaFX;
 
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
     public class App extends Application {
         @Override
         public void start(Stage primaryStage) throws Exception {
+            System.out.println(getClass().getResource("/MainApp.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/MainApp.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
             primaryStage.setTitle("My First JavaFX App");
+            primaryStage.setScene(scene);
             primaryStage.show();
         }
 
---
+++ src/main/java/ru/brigada/javaFX/controller/MainController.java
@@ -0,0 +1,4 @@
+package ru.brigada.javaFX.controller;
+
+public class MainController {
+}
